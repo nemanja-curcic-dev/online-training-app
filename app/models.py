791bc1365db0c41e3ff7449916f999bd6d1cb764
nemanja-curcic-dev <curesr@gmail.com>
2017-08-06 13:08:39 +0200

    added first two training session tables

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from datetime import datetime
 
 
+#user class
 class Users(db.Model):
     __tablename__ = 'users'
 
@@ -13,3 +14,27 @@ class Users(db.Model):
     date_registered = db.Column(db.DateTime(), default=datetime.utcnow)
     confirmed = db.Column(db.Boolean, default=False)
     is_administrator = db.Column(db.Boolean, default=False)
+
+
+#training session classes
+class TrainingSession(db.Model):
+    __tablename__ = 'training_session'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    date_created = db.Column(db.DateTime())
+    training_goal = db.Column(db.String(60))
+    training_type = db.Column(db.Integer)
+    session_exercises = db.relationship('TrainingSessionExercises', backref='training_session', lazy='dynamic')
+
+
+class TrainingSessionExercises(db.Model):
+    __tablename__ = 'training_session_exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    training_session_id = db.Column(db.Integer, db.ForeignKey('training_session.id'))
+    training_part = db.Column(db.SmallInteger)
+    exercise = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    reps = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
